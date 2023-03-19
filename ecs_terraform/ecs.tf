27c5763c3fccf1af222a5f489494b75f9451446f
Gitlab Runner <noreply@paloaltonetworks.com>
2023-03-20 03:11:34 +0530

    test

--- ecs_terraform/ecs.tf
+++ ecs_terraform/ecs.tf
@@ -6,9 +6,9 @@ resource "aws_ecs_cluster" "web-cluster" {
   }
 }
 
-resource "aws_iam_service_linked_role" "ecs" {
-  aws_service_name = "ecs.amazonaws.com"
-}
+# resource "aws_iam_service_linked_role" "ecs" {
+#   aws_service_name = "ecs.amazonaws.com"
+# }
 
 resource "aws_ecs_capacity_provider" "test" {
   name = "capacity-provider-test"
