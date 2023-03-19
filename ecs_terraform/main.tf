dc3bcd01681c47bf73c8ee8b82e2fe6e9eb6ec59
Gitlab Runner <noreply@paloaltonetworks.com>
2023-03-20 01:59:07 +0530

    added s3 bucket

--- ecs_terraform/main.tf
+++ ecs_terraform/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region  = "us-east-1"
-  shared_credentials_file = "$HOME/.aws/credentials" # default
-  profile = "default" # you may change to desired profile
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile = "default"
   version = "~> 2.63"
 }
 
 
 terraform {
   backend "s3" {
-    bucket = "ecsworkshopbucket00"
+    bucket = "ecs99088"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
