7f56bd860215850334cb25f462a6b9854996c4a8
Gitlab Runner <noreply@paloaltonetworks.com>
2023-03-20 02:29:24 +0530

    test

--- ecs_terraform/main.tf
+++ ecs_terraform/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
-  # shared_credentials_file = "$HOME/.aws/credentials"
+  shared_credentials_file = "$HOME/.aws/credentials"
   profile = "default"
   # version = "~> 0.12.6"
 }
